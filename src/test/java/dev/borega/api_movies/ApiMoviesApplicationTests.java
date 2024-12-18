a02b7d3d6428fa9ec56512afebead352f437e2a8
Borega8 <borega888@gmail.com>
2024-12-18 12:42:30 -0600

    chore: first commit

---
+++ src/test/java/dev/borega/api_movies/ApiMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.borega.api_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
