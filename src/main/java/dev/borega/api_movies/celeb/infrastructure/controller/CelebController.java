abf2d15b5558e766074747de42928c0067ebd339
Borega8 <borega888@gmail.com>
2024-12-24 17:50:51 -0600

    refactor(celeb): use new APIRespose object and celeb mapper

--- src/main/java/dev/borega/api_movies/celeb/infrastructure/controller/CelebController.java
+++ src/main/java/dev/borega/api_movies/celeb/infrastructure/controller/CelebController.java
@@ -3,7 +3,10 @@ package dev.borega.api_movies.celeb.infrastructure.controller;
 import dev.borega.api_movies.celeb.aplication.CelebPort;
 import dev.borega.api_movies.celeb.domain.exception.CelebNotFoundException;
 import dev.borega.api_movies.celeb.domain.model.Celeb;
+import dev.borega.api_movies.celeb.infrastructure.dto.BasicCelebDTO;
+import dev.borega.api_movies.celeb.infrastructure.mapper.CelebMapper;
 import dev.borega.api_movies.shared.domain.exception.InvalidValueException;
+import dev.borega.api_movies.shared.domain.model.APIResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,55 +20,63 @@ import java.util.List;
 public class CelebController {
 
     private final CelebPort celebPort;
+    private final CelebMapper celebMapper = CelebMapper.INSTANCE;
 
     @GetMapping
-    public ResponseEntity<List<Celeb>> getAllCelebs() {
-        List<Celeb> celebs = celebPort.getAll();
-        return ResponseEntity.ok(celebs);
+    public ResponseEntity<APIResponse<List<BasicCelebDTO>>> getAllCelebs() {
+        List<BasicCelebDTO> celebs = celebPort.getAll().stream().map(celebMapper::celebToBasicCelebDTO).toList();
+        APIResponse<List<BasicCelebDTO>> res = new APIResponse<>(celebs);
+        return ResponseEntity.ok(res);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Celeb> getCeleb(@PathVariable Long id) {
+    public ResponseEntity<APIResponse<BasicCelebDTO>> getCeleb(@PathVariable Long id) {
         try {
             Celeb celeb = celebPort.getById(id);
-            return ResponseEntity.ok(celeb);
+            APIResponse<BasicCelebDTO> res = new APIResponse<>(celebMapper.celebToBasicCelebDTO(celeb));
+            return ResponseEntity.ok(res);
 
         } catch (Exception e) {
             if (e instanceof CelebNotFoundException)
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse<>(null, e.getMessage()));
 
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new APIResponse<>(null, e.getMessage()));
         }
     }
 
     @PostMapping
-    public ResponseEntity<?> saveCeleb(@RequestBody Celeb celeb) {
+    public ResponseEntity<?> saveCeleb(@RequestBody BasicCelebDTO celebDTO) {
         try {
-            Celeb newCeleb = celebPort.save(celeb);
+            Celeb celeb = celebPort.save(celebMapper.basicCelebDTOToCeleb(celebDTO));
 
-            return ResponseEntity.status(HttpStatus.CREATED).body(newCeleb);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(new APIResponse<>(celebMapper.celebToBasicCelebDTO(celeb)));
         } catch (Exception e) {
             if (e instanceof InvalidValueException)
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse<>(null, e.getMessage()));
 
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new APIResponse<>(null, e.getMessage()));
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateCeleb(@PathVariable Long id, @RequestBody Celeb celeb) {
+    public ResponseEntity<?> updateCeleb(@PathVariable Long id, @RequestBody BasicCelebDTO celebDTO) {
         try {
+            Celeb celeb = celebMapper.basicCelebDTOToCeleb(celebDTO);
+            celeb.setId(id);
+
             Celeb celebUpdated = celebPort.update(celeb);
-            return ResponseEntity.ok(celebUpdated);
+            return ResponseEntity.ok(new APIResponse<>(celebMapper.celebToBasicCelebDTO(celebUpdated)));
 
         } catch (Exception e) {
             if (e instanceof CelebNotFoundException)
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse<>(null, e.getMessage()));
 
             if (e instanceof InvalidValueException)
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse<>(null, e.getMessage()));
 
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new APIResponse<>(null, "Unexpected Error"));
         }
     }
 
@@ -77,9 +88,9 @@ public class CelebController {
 
         } catch (Exception e) {
             if (e instanceof CelebNotFoundException)
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse<>(null, e.getMessage()));
 
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new APIResponse<>(null, "Unexpected Error"));
         }
     }
 
