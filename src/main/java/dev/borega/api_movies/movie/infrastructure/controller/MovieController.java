bc45cb1c8bb5e5d650ac852d017f481811ae7195
Borega8 <borega888@gmail.com>
2024-12-24 18:24:47 -0600

    refactor(movie): use APIResponse object

--- src/main/java/dev/borega/api_movies/movie/infrastructure/controller/MovieController.java
+++ src/main/java/dev/borega/api_movies/movie/infrastructure/controller/MovieController.java
@@ -6,6 +6,7 @@ import dev.borega.api_movies.shared.domain.exception.InvalidValueException;
 import dev.borega.api_movies.movie.domain.exception.MovieNotFoundException;
 import dev.borega.api_movies.movie.domain.model.MPAClassification;
 import dev.borega.api_movies.movie.domain.model.Movie;
+import dev.borega.api_movies.shared.domain.model.APIResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,30 +22,30 @@ public class MovieController {
     private final MoviePort moviePort;
 
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies(
+    public ResponseEntity<APIResponse<List<Movie>>> getAllMovies(
             @RequestParam(required = false) String title,
             @RequestParam(required = false) MPAClassification classification
     ) {
         if (classification != null && title != null)
-            return ResponseEntity.ok(moviePort.getByTitleAndClassification(title, classification));
+            return ResponseEntity.ok(new APIResponse<>(moviePort.getByTitleAndClassification(title, classification)));
 
         if (classification != null)
-            return ResponseEntity.ok(moviePort.getByClassification(classification));
+            return ResponseEntity.ok(new APIResponse<>(moviePort.getByClassification(classification)));
 
         if (title != null)
-            return ResponseEntity.ok(moviePort.getByTitle(title));
+            return ResponseEntity.ok(new APIResponse<>(moviePort.getByTitle(title)));
 
-        return ResponseEntity.ok(moviePort.getAll());
+        return ResponseEntity.ok(new APIResponse<>(moviePort.getAll()));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Movie> getMovie(@PathVariable Long id) {
+    public ResponseEntity<APIResponse<Movie>> getMovie(@PathVariable Long id) {
         try {
-            return ResponseEntity.ok(moviePort.getById(id));
+            return ResponseEntity.ok(new APIResponse<>(moviePort.getById(id)));
 
         } catch (Exception e) {
             if (e instanceof MovieNotFoundException)
-                return ResponseEntity.notFound().build();
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse<>(null, e.getMessage()));
 
             return ResponseEntity.internalServerError().build();
         }
@@ -53,14 +54,14 @@ public class MovieController {
     @PostMapping
     public ResponseEntity<?> saveMovie(@RequestBody Movie movie) {
         try {
-            return ResponseEntity.status(HttpStatus.CREATED).body(moviePort.save(movie));
+            return ResponseEntity.status(HttpStatus.CREATED).body(new APIResponse<>(moviePort.save(movie)));
 
         } catch (Exception e) {
             if (e instanceof InvalidValueException)
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse<>(null, e.getMessage()));
 
             if (e instanceof IllegalArgumentException)
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse<>(null, e.getMessage()));
 
 
             return ResponseEntity.internalServerError().build();
@@ -70,17 +71,17 @@ public class MovieController {
     @PutMapping("/{id}")
     public ResponseEntity<?> updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
         try {
-            return ResponseEntity.ok(moviePort.update(movie));
+            return ResponseEntity.ok(new APIResponse<>(moviePort.update(movie)));
 
         } catch (Exception e) {
             if (e instanceof MovieNotFoundException)
-                return ResponseEntity.notFound().build();
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse<>(null, e.getMessage()));
 
             if (e instanceof InvalidValueException)
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse<>(null, e.getMessage()));
 
             if (e instanceof IllegalArgumentException)
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new APIResponse<>(null, e.getMessage()));
 
             return ResponseEntity.internalServerError().build();
         }
@@ -94,7 +95,7 @@ public class MovieController {
 
         } catch (Exception e) {
             if (e instanceof CelebNotFoundException)
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse<>(null, e.getMessage()));
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
