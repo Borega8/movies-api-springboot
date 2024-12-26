b75b00f58f9cc952ff4c2b46d73d5dc602cf68b6
Borega8 <borega888@gmail.com>
2024-12-26 12:55:02 -0600

    feat(company): add company post endpoint

--- src/main/java/dev/borega/api_movies/company/infrastructure/controller/CompanyController.java
+++ src/main/java/dev/borega/api_movies/company/infrastructure/controller/CompanyController.java
@@ -2,16 +2,15 @@ package dev.borega.api_movies.company.infrastructure.controller;
 
 import dev.borega.api_movies.company.application.CompanyPort;
 import dev.borega.api_movies.company.domain.exception.CompanyNotFoundException;
+import dev.borega.api_movies.company.domain.model.Company;
 import dev.borega.api_movies.company.infrastructure.dto.BasicCompanyDTO;
 import dev.borega.api_movies.company.infrastructure.mapper.CompanyMapper;
+import dev.borega.api_movies.shared.domain.exception.InvalidValueException;
 import dev.borega.api_movies.shared.domain.model.APIResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -50,6 +49,26 @@ public class CompanyController {
                         .status(HttpStatus.NOT_FOUND)
                         .body(new APIResponse<>(null, e.getMessage())
                         );
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<APIResponse<BasicCompanyDTO>> saveCompany(@RequestBody BasicCompanyDTO companyDTO) {
+        try {
+            Company company = companyPort.save(companyMapper.basicCompanyDTOToCompany(companyDTO));
+
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(new APIResponse<>(
+                            companyMapper.companyToBasicCompanyDTO(company)
+                    ));
+        } catch (Exception e) {
+            if (e instanceof InvalidValueException)
+                return ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(new APIResponse<>(null, e.getMessage()));
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
