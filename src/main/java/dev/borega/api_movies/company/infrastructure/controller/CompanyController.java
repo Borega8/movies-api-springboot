44a10a9d04a5a3a5775079e317a3df906270df73
Borega8 <borega888@gmail.com>
2024-12-26 12:46:03 -0600

    feat(company): add get company by id endpoint

--- src/main/java/dev/borega/api_movies/company/infrastructure/controller/CompanyController.java
+++ src/main/java/dev/borega/api_movies/company/infrastructure/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package dev.borega.api_movies.company.infrastructure.controller;
 
 import dev.borega.api_movies.company.application.CompanyPort;
+import dev.borega.api_movies.company.domain.exception.CompanyNotFoundException;
 import dev.borega.api_movies.company.infrastructure.dto.BasicCompanyDTO;
 import dev.borega.api_movies.company.infrastructure.mapper.CompanyMapper;
 import dev.borega.api_movies.shared.domain.model.APIResponse;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,25 @@ public class CompanyController {
                                 .map(companyMapper::companyToBasicCompanyDTO)
                                 .toList()
                 ));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<APIResponse<BasicCompanyDTO>> getCompanyById(@PathVariable Long id) {
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new APIResponse<>(
+                            companyMapper.companyToBasicCompanyDTO(companyPort.getById(id))
+                        )
+                    );
+        } catch (Exception e) {
+            if (e instanceof CompanyNotFoundException)
+                return ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .body(new APIResponse<>(null, e.getMessage())
+                        );
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
