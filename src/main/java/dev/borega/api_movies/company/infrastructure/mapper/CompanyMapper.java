5fb441cfd4baa04be3920a7087d1622d74a6b728
Borega8 <borega888@gmail.com>
2024-12-26 12:06:20 -0600

    feat(company): create company mapper

---
+++ src/main/java/dev/borega/api_movies/company/infrastructure/mapper/CompanyMapper.java
@@ -0,0 +1,14 @@
+package dev.borega.api_movies.company.infrastructure.mapper;
+
+import dev.borega.api_movies.company.domain.model.Company;
+import dev.borega.api_movies.company.infrastructure.persistence.entity.CompanyEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CompanyMapper {
+    CompanyMapper INSTANCE = Mappers.getMapper(CompanyMapper.class);
+
+    Company companyEntityToCompany(CompanyEntity companyEntity);
+    CompanyEntity companyToCompanyEntity(Company company);
+}
