e75c372172f273c3e6357ff22dad94434485d92a
Borega8 <borega888@gmail.com>
2024-12-27 18:24:21 -0600

    fix(celeb): fix mapping between domain and entity celeb

--- src/main/java/dev/borega/api_movies/celeb/infrastructure/mapper/CelebMapper.java
+++ src/main/java/dev/borega/api_movies/celeb/infrastructure/mapper/CelebMapper.java
@@ -4,6 +4,7 @@ import dev.borega.api_movies.celeb.domain.model.Celeb;
 import dev.borega.api_movies.celeb.infrastructure.dto.BasicCelebDTO;
 import dev.borega.api_movies.celeb.infrastructure.persistence.entity.CelebEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -13,6 +14,13 @@ public interface CelebMapper {
     BasicCelebDTO celebToBasicCelebDTO(Celeb celeb);
     Celeb basicCelebDTOToCeleb(BasicCelebDTO basicCelebDTO);
 
+    @Mapping(source = "celebId", target = "id")
+    @Mapping(source = "name", target = "fullName")
+    @Mapping(source = "placeOfBirth", target = "birthPlace")
     Celeb celebEntityToCeleb(CelebEntity celebEntity);
+
+    @Mapping(source = "id", target = "celebId")
+    @Mapping(source = "fullName", target = "name")
+    @Mapping(source = "birthPlace", target = "placeOfBirth")
     CelebEntity celebToCelebEntity(Celeb celeb);
 }
