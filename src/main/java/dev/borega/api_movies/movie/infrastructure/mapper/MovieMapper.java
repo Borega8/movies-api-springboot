a0328af9bee58df4e1d1c1666918f1f5880248cb
Borega8 <borega888@gmail.com>
2025-01-05 19:22:31 -0600

    fix(movie): fix movie mapper methods

--- src/main/java/dev/borega/api_movies/movie/infrastructure/mapper/MovieMapper.java
+++ src/main/java/dev/borega/api_movies/movie/infrastructure/mapper/MovieMapper.java
@@ -1,8 +1,13 @@
 package dev.borega.api_movies.movie.infrastructure.mapper;
 
+import dev.borega.api_movies.celeb.domain.model.Celeb;
+import dev.borega.api_movies.celeb.infrastructure.dto.CelebNameDTO;
+import dev.borega.api_movies.celeb.infrastructure.mapper.CelebMapper;
+import dev.borega.api_movies.celeb.infrastructure.persistence.entity.CelebEntity;
 import dev.borega.api_movies.movie.domain.model.Movie;
 import dev.borega.api_movies.movie.infrastructure.dto.BasicMovieDTO;
 import dev.borega.api_movies.movie.infrastructure.persistence.entity.MovieEntity;
+import dev.borega.api_movies.movie.infrastructure.persistence.entity.MoviesHasCelebs;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -16,4 +21,42 @@ public interface MovieMapper {
 
     MovieEntity movieToMovieEntity(Movie movie);
     Movie movieEntityToMovie(MovieEntity movieEntity);
+
+    default MoviesHasCelebs celebToMoviesHasCelebs(Celeb celeb) {
+        if ( celeb == null ) {
+            return null;
+        }
+
+        CelebEntity celebEntity = CelebMapper.INSTANCE.celebToCelebEntity(celeb);
+        MoviesHasCelebs moviesHasCelebs = new MoviesHasCelebs();
+
+        moviesHasCelebs.setCeleb(celebEntity);
+
+        return moviesHasCelebs;
+    }
+
+    default Celeb moviesHasCelebsToCeleb(MoviesHasCelebs moviesHasCelebs) {
+        if ( moviesHasCelebs == null ) {
+            return null;
+        }
+
+        Celeb celeb = new Celeb();
+
+        celeb.setId(moviesHasCelebs.getCeleb().getCelebId());
+        celeb.setFullName(moviesHasCelebs.getCeleb().getName());
+        celeb.setBirthDate(moviesHasCelebs.getCeleb().getBirthDate());
+        celeb.setBirthPlace(moviesHasCelebs.getCeleb().getPlaceOfBirth());
+        celeb.setBiography(moviesHasCelebs.getCeleb().getBiography());
+        celeb.setPhoto(moviesHasCelebs.getCeleb().getPhoto());
+
+        return celeb;
+    }
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "fullName", target = "name")
+    CelebNameDTO celebToCelebNameDTO(Celeb celeb);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "fullName")
+    Celeb celebNameDTOToCeleb(CelebNameDTO celebNameDTO);
 }
