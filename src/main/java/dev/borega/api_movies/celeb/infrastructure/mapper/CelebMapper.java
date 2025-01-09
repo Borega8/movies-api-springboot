7b1aeb9117b3b6b0c2cbb9c93037f144f93b37a6
Borega8 <borega888@gmail.com>
2025-01-08 18:18:24 -0600

    fix(celeb): fix mapping to get celeb's movies

--- src/main/java/dev/borega/api_movies/celeb/infrastructure/mapper/CelebMapper.java
+++ src/main/java/dev/borega/api_movies/celeb/infrastructure/mapper/CelebMapper.java
@@ -3,9 +3,18 @@ package dev.borega.api_movies.celeb.infrastructure.mapper;
 import dev.borega.api_movies.celeb.domain.model.Celeb;
 import dev.borega.api_movies.celeb.infrastructure.dto.BasicCelebDTO;
 import dev.borega.api_movies.celeb.infrastructure.persistence.entity.CelebEntity;
+import dev.borega.api_movies.company.domain.model.CompanyInfo;
+import dev.borega.api_movies.movie.domain.model.MPAClassification;
+import dev.borega.api_movies.movie.domain.model.Movie;
+import dev.borega.api_movies.movie.domain.model.MovieGenre;
+import dev.borega.api_movies.movie.infrastructure.mapper.MovieMapper;
+import dev.borega.api_movies.movie.infrastructure.persistence.entity.MoviesHasCelebs;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Mapper
 public interface CelebMapper {
@@ -23,4 +32,31 @@ public interface CelebMapper {
     @Mapping(source = "fullName", target = "name")
     @Mapping(source = "birthPlace", target = "placeOfBirth")
     CelebEntity celebToCelebEntity(Celeb celeb);
+
+    default Movie moviesHasCelebsToMovie(MoviesHasCelebs moviesHasCelebs) {
+        if ( moviesHasCelebs == null ) {
+            return null;
+        }
+
+        Long id = moviesHasCelebs.getMovie().getId();
+
+        String title = moviesHasCelebs.getMovie().getTitle();
+        LocalDate releaseDate = moviesHasCelebs.getMovie().getReleaseDate();
+        String poster = moviesHasCelebs.getMovie().getPoster();
+        Double rating = moviesHasCelebs.getMovie().getRating();
+
+        return new Movie(id, title, null, null, releaseDate, poster, null, rating, null, null, null);
+    }
+
+    default MoviesHasCelebs movieToMoviesHasCelebs(Movie movie) {
+        if ( movie == null ) {
+            return null;
+        }
+
+        MoviesHasCelebs moviesHasCelebs = new MoviesHasCelebs();
+
+        moviesHasCelebs.setMovie(MovieMapper.INSTANCE.movieToMovieEntity(movie));
+
+        return moviesHasCelebs;
+    }
 }
