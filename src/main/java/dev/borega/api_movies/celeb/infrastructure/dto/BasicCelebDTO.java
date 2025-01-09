bbc6cb8f974ab50c66eb4182c5b72242428d8024
Borega8 <borega888@gmail.com>
2025-01-08 18:15:46 -0600

    feat(celeb): update dto showing celeb's movies

--- src/main/java/dev/borega/api_movies/celeb/infrastructure/dto/BasicCelebDTO.java
+++ src/main/java/dev/borega/api_movies/celeb/infrastructure/dto/BasicCelebDTO.java
@@ -1,7 +1,14 @@
 package dev.borega.api_movies.celeb.infrastructure.dto;
 
 import java.time.LocalDate;
+import java.util.Set;
 
-public record BasicCelebDTO(String fullName, LocalDate birthDate, String birthPlace, String biography,
-                            String photo) {
+public record BasicCelebDTO(
+        String fullName,
+        LocalDate birthDate,
+        String birthPlace,
+        String biography,
+        String photo,
+        Set<CelebMovieDTO> movies
+) {
 }
